8f40d3cb0c16b996c650432ca311679fbc49b77f
xswordsx <xswordsx@gmail.com>
2014-01-19 13:47:28 +0200

    Change 'harmonic' to use a block instead of a C-style approach

--- solutions/01.rb
+++ solutions/01.rb
@@ -5,8 +5,18 @@ class Integer
     (2..Math.sqrt(self)).all? {|x| remainder(x).nonzero?}
   end
 
+  def prime_factors
+    return [] if self == 1
+    factor = (2..abs).find {|x| x.prime? and remainder(x).zero?}
+    [factor] + (abs / factor).prime_factors
+  end
+
   def digits
-    abs.to_s.chars.map(&to_i)
+    abs.to_s.chars.map(&:to_i)
+  end
+
+  def harmonic
+    (1..self).reduce {|sum, x| sum + Rational(1, x)}
   end
 
 end
