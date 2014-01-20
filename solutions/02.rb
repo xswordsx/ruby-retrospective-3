c9212eead4c46aa3daec7ff731805c0a186efdaf
xswordsx <xswordsx@gmail.com>
2014-01-20 13:51:43 +0200

    First commit for second solution

---
+++ solutions/02.rb
@@ -0,0 +1,103 @@
+module Constants
+  STATUS = {
+    "TODO"f => :todo,
+    "DONE"f => :done,
+    "CURRENT"f => :current
+    }.freeze
+
+  PRIORITY = {
+    "High"f => :high,
+    "Normal"f => :normal,
+    "Low"f => :low
+    }.freeze
+
+end
+
+class TodoList
+
+  def initialize(item_array)
+    @array = item_array.select {|elem| elem.kind_of? TodoItem}
+  end
+
+  def each(&block)
+    @array.each {|x| yield x}
+  end
+
+  def TodoList.parse(todo_rows)
+    return_list = []
+    todo_rows.split("\n").each {|row| return_list << TodoItem.new(row.to_s)}
+    TodoList.new(return_list)
+  end
+
+  def filter(criteria)
+  end
+
+  def tasks_todo
+    @array.count {|x| x.status == :todo}
+  end
+
+  def tasks_in_progress
+    @array.count {|x| x.status == :current}
+  end
+
+  def tasks_done
+    @array.count{|x| x.status == :done}
+  end
+
+  def completed?
+    @array.length == tasks_done
+  end
+
+  def filter(criteria)
+    @array.select {|x| criteria.call(x)}
+  end
+
+end
+
+
+class TodoItem
+
+  attr_reader :tags
+  attr_reader :description
+
+  def initialize(row)
+    stripped_list = []
+    row.split('|').each {|x| stripped_list << x.strip}
+
+    @status, @description, @priority, @tags = stripped_list
+    @tags = stripped_list.pop.split(',') if @tags
+  end
+
+  def to_s
+    "#{@status} | #{@description} | #{@priority} | #{@tags}"
+  end
+
+  def status
+    Constants::STATUS[@status]
+  end
+
+  def priority
+    Constants::PRIORITY[@priority]
+  end
+
+end
+
+
+class Criteria
+  @operator = :==
+  class << self
+    def status(status)
+      Proc.new {|obj| obj.status.send(@operator, status)}
+    end
+
+    def priority(priority)
+      Proc.new {|obj| obj.priority.send(@operator, priority)}
+    end
+
+    def tags(tags)
+      Proc.new {|obj| obj.tags.send(@operator, tags)}
+    end
+
+
+  end
+end
